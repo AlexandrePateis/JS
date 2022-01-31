ebe02a7e18da88a3beae53ec90c253d53ada9671
alexandre <alepateis9090@gmail.com>
2022-01-31 19:25:03 -0300

    melhorando

--- script.js
+++ script.js
@@ -5,18 +5,19 @@ function carregar(){
     var hora = date.getHours()
     msg.innerHTML = `Agora sao ${hora} horas`
     if (hora >= 0 && hora < 12){
-        msg.innerHTML = 'Good Morning !!'
+        msg.innerHTML = `Good Morning !! Now are ${hora} a'clock'`
         img.src = 'manha.jpg'
         document.body.style.background = '#b5b6b1'
     }else if (hora >=12 && hora <= 18){
         //Boa tarde
-        msg.innerHTML = 'Good Afternoon !!'
+        msg.innerHTML = `Good Aftrnoon !! Now are ${hora} a'clock`
         img.src = 'tarde.jpg'
         document.body.style.background = '#191627'
     }else{
         //Boa noite 
-        msg.innerHTML = 'Good Night !!'
+        msg.innerHTML = `Good Night !! Now are ${hora} a'clock`
         img.src = 'noite.jpg'
         document.body.style.background = '#653f74'
     }
 }
+
