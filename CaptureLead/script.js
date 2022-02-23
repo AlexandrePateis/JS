6b38960768be296b4c47c9be5ebee84239e723fa
alexandre <alepateis9090@gmail.com>
2022-02-23 20:01:44 -0300

    reticano Capture Lead

--- CaptureLead/script.js
+++ CaptureLead/script.js
@@ -6,38 +6,45 @@ formulario.addEventListener('submit' , function(e) {
     const sobrenome = document.getElementById("last-name");
     const email = document.getElementById("email");
     const senha = document.getElementById("pass");
+    todosVazios()
     
-    // verificar se o nome está vazio
-    if (nome.value == "") {
-      alert("Nome não informado");
-      e.preventDefault();
-      // Deixa o input com o focus
-      nome.focus();
-      // retorna a função e não olha as outras linhas
-      return;
+    function todosVazios() {
+      if(nome.value == '' && sobrenome.value == '' && email.value == '' && senha.value == ''){
+        alert('Nenhum dado informado, tente novamente.');
+        e.preventDefault();
+        return
+      }
+      else{
+        if (nome.value == "") {
+          alert("Nome não informado");
+          e.preventDefault();
+          nome.focus();
+          return;
+        }
+    
+        if (sobrenome.value == "") {
+          alert("Sobrenome não informado");
+          sobrenome.focus();
+          e.preventDefault();
+          return;
+        }
+    
+        if (email.value == "") {
+          alert("E-mail não informado");
+          email.focus();
+          e.preventDefault();
+          return;
+        }
+        if (senha.value == "") {
+          alert("Senha não informada");
+          e.preventDefault();
+          senha.focus();
+          return;
+        }
+      }
+      alert("Formulário enviado!");
     }
-    if (sobrenome.value == "") {
-      alert("Sobrenome não informado");
-      sobrenome.focus();
-      e.preventDefault();
-      return;
-    }
-    if (email.value == "") {
-      alert("E-mail não informado");
-      email.focus();
-      e.preventDefault();
-      return;
-    }
-    if (senha.value == "") {
-      alert("Senha não informada");
-      e.preventDefault();
-      senha.focus();
-      return;
-    }
-
-    alert("Formulário enviado!");
-    // envia o formulário
-    //formulario.submit();
+    
 });
 
   
