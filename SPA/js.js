7da5cb5e764e0d24567660128b0e4a5da11a3a12
alexandre <alepateis9090@gmail.com>
2022-02-24 22:16:44 -0300

    SPA

---
+++ SPA/js.js
@@ -0,0 +1,8 @@
+const formulario = document.getElementById('form')
+formulario.addEventListener('submit', function(e) {
+    const nome = document.getElementById('nome')
+    const email = document.getElementById('email')
+    const cidade = document.getElementById('cidade')
+    const resultado = document.getElementById('pega-resultado')
+    const termos = document.getElementById('termos')
+})
